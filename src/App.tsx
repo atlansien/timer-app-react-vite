e57d8d2031952f7471917d8b88b04344c0dde0f6
高田柊輝 <takatatoki@takadahiiragiakiranoMacBook-Air.local>
2022-03-04 22:14:48 +0900

    update: css調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,68 @@
-import { useEffect, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import { Button, Stack } from '@mui/material';
+import './App.css';
 
 function App() {
-  const [time, setTime] = useState(0)
+  const [time, setTime] = useState(0);
 
   const [start, setStart] = useState(false);
 
   useEffect(() => {
-    let interval: number | undefined
-    if(start) {
+    let interval: number | undefined;
+    if (start) {
       interval = setInterval(() => {
-        setTime(prevTime => prevTime + 10)
-      }, 1)
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
     } else {
-      clearInterval(interval)
+      clearInterval(interval);
     }
 
-    return () => clearInterval(interval)
-  }, [start])
+    return () => clearInterval(interval);
+  }, [start]);
 
   const resetTimeHandler = () => {
-    setTime(0)
-  }
-
+    setTime(0);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header
+        className="App-header"
+        style={{ backgroundColor: time >= 180000 ? 'orangered' : 'aquamarine' }}
+      >
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <span>{ ("0" + Math.floor(time/1000/60)%60).slice(-2)}</span>:
-          <span>{("0" + Math.floor(time/1000)%60).slice(-2)}</span>
+        <p
+          className="time"
+          style={{ color: time >= 180000 ? 'lightyellow' : 'royalblue' }}
+        >
+          <span>{('0' + (Math.floor(time / 1000 / 60) % 60)).slice(-2)}</span>:
+          <span>{('0' + (Math.floor(time / 1000) % 60)).slice(-2)}</span>
         </p>
-        <button onClick={() => setStart(true)}>click</button>
-        <button onClick={() => setStart(false)}>click</button>
-        <button onClick={resetTimeHandler}>click</button>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            onClick={() => setStart(true)}
+          >
+            START
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="inherit"
+            onClick={() => setStart(false)}
+          >
+            STOP
+          </Button>
+          <Button variant="contained" size="large" color="error" onClick={resetTimeHandler}>
+            RESET
+          </Button>
+        </Stack>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
