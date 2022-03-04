f2393fc1f146f828f016f81906f0a3093ef14a2a
高田柊輝 <takatatoki@takadahiiragiakiranoMacBook-Air.local>
2022-03-04 23:00:07 +0900

    update: 背景色の変わるタイミングを任意で調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import { Button, Stack } from '@mui/material';
+import { Button, Stack, TextField } from '@mui/material';
 import './App.css';
 
 function App() {
   const [time, setTime] = useState(0);
 
   const [start, setStart] = useState(false);
+  const [second, setSecond] = useState(180);
+  const [milliSecond, setMilliSecond] = useState(180000)
 
   useEffect(() => {
     let interval: number | undefined;
@@ -21,6 +23,10 @@ function App() {
     return () => clearInterval(interval);
   }, [start]);
 
+  useEffect(() => {
+    setMilliSecond(second * 1000);
+  }, [second, milliSecond]);
+
   const resetTimeHandler = () => {
     setTime(0);
   };
@@ -29,12 +35,14 @@ function App() {
     <div className="App">
       <header
         className="App-header"
-        style={{ backgroundColor: time >= 180000 ? 'orangered' : 'aquamarine' }}
+        style={{
+          backgroundColor: time >= milliSecond ? 'orangered' : 'aquamarine',
+        }}
       >
         <img src={logo} className="App-logo" alt="logo" />
         <p
           className="time"
-          style={{ color: time >= 180000 ? 'lightyellow' : 'royalblue' }}
+          style={{ color: time >= milliSecond ? 'lightyellow' : 'royalblue' }}
         >
           <span>{('0' + (Math.floor(time / 1000 / 60) % 60)).slice(-2)}</span>:
           <span>{('0' + (Math.floor(time / 1000) % 60)).slice(-2)}</span>
@@ -56,9 +64,23 @@ function App() {
           >
             STOP
           </Button>
-          <Button variant="contained" size="large" color="error" onClick={resetTimeHandler}>
+          <Button
+            variant="contained"
+            size="large"
+            color="error"
+            onClick={resetTimeHandler}
+          >
             RESET
           </Button>
+        </Stack>
+        <Stack style={{margin: '10px'}} direction="row" spacing={2}>
+          <TextField
+            id="outlined-size-small"
+            size="small"
+            value={second}
+            type="number"
+            onChange={event => setSecond(Number(event.target.value))}
+          />
         </Stack>
       </header>
     </div>
